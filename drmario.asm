7b4f2ae1ff0bf516e79027b1792a40572d5ad1b1
ethanliu24 <liu.ethan2005@gmail.com>
2025-03-21 15:43:43 -0400

    clean up

--- drmario.asm
+++ drmario.asm
@@ -70,9 +70,7 @@ FALLING:  # player can control the capsule
 
 # bash related vairables. the length is the # of bytes to write in the command file
 INSTR_FILE:
-    .asciiz "/Users/ethanliu/projects/dr-mario-asm/bash_instr.txt"
-SPACE:
-    .asciiz ""
+    .asciiz "PASTE-THE-ABSOLUTE-PATH-FROM-BASH-HERE"
 
 ROTATE_SFX:
     .asciiz "rotate.mp3"

e7edbf5b3ae5e863ed7f75eede4d54098e9012cc
ethanliu24 <liu.ethan2005@gmail.com>
2025-03-21 00:48:41 -0400

    modify bash for extra skip cmd and create the instr txt file when running

--- listener.bash
+++ listener.bash
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 PID=$$
+INSTR_FILE="bash_instr.txt"
+
+touch $INSTR_FILE
+ABS_PATH=$(pwd)
+ABS_PATH+="/"
+ABS_PATH+=$INSTR_FILE
+echo "Copy and paste this path to INSTR_FILE variable: $ABS_PATH"
 
 while true; do
-    cmd=$(cat bash_instruction.txt)
+    cmd=$(cat $INSTR_FILE)
 
-    if [ "$cmd" = "EXIT" ];
-    then
+    if [ "$cmd" = "SKIP" ]; then
+        continue
+    elif [ "$cmd" = "EXIT" ]; then
         kill $PID
         break
     else  # a music file
+        # afplay works on mac, not sure about linux but def not windows
         afplay "$cmd" > /dev/null 2>&1
     fi
 done
